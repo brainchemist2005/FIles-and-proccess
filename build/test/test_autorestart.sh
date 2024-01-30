5633d0f3849085272cc44d67f3fdb391ebe2219c
brainchemist2005 <zackariaebouargan@gmail.com>
2024-01-30 18:25:47 -0500

    Removed the build file and modified the gitignore

--- build/test/test_autorestart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -x
-# Script généré automatiquement à partir de test_autorestart.sh.in
-
-BASE=/home/rabahlemaici/Bureau/FIles-and-proccess/build/bin
-export PATH="$BASE:$PATH"
-
-autorestart montecarlo | tee log.txt
-
-grep -i "Success" log.txt
