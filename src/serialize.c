a52b600203bbaccffb6fec22d307f80bc690c6f0
lemaicirabah <lemaici.rabah@courrier.uqam.ca>
2024-01-30 11:02:41 -0500

    ajoute save_state

--- src/serialize.c
+++ src/serialize.c
@@ -9,24 +9,24 @@
 // TODO: sauvegarder la structure state dans le fichier spécifié.
 int save_state(struct state* s, const char* filename) {
 
-  FILE *file;
+  int file;
 
-  file = open(FILENAME, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | O_BINARY);
+  file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-  if (file_descriptor == -1) {
+  if (file == -1) {
     perror("An error occurred creating the file");
     exit(EXIT_FAILURE);
   }
 
-  printf("%ld %ld\n", s->points_total, s->points_inside);
+  printf("HELLOOOOO%ld %ld\n", s->points_total, s->points_inside);
 
   // Write the struct members to the file using write
-  write(file_descriptor, &(s->points_inside), sizeof(long));
-  write(file_descriptor, &(s->points_total), sizeof(long));
-  write(file_descriptor, &(s->num_restart), sizeof(int));
+  write(file, &(s->points_inside), sizeof(long));
+  write(file, &(s->points_total), sizeof(long));
+  write(file, &(s->num_restart), sizeof(int));
 
   // Close the file
-  close(file_descriptor);
+  close(file);
   return 0;
 }
 
