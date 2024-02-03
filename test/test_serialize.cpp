028dcb8c1f96f02762b7c4cdf32cfe84c9628e33
lemaicirabah <lemaici.rabah@courrier.uqam.ca>
2024-02-03 00:26:19 -0500

    Ajout de test unitaire tache2

--- test/test_serialize.cpp
+++ test/test_serialize.cpp
@@ -13,5 +13,13 @@ static const char* k_test_file = "test.bin";
 
 TEST(Serialize, SaveLoad) {
   // TODO
-  EXPECT_EQ(0, 1);
+  struct state s1 {
+    11, 22, 33
+  };
+  struct state s2;
+  save_state(&s1,k_test_file);
+  load_state(&s2,k_test_file);
+  EXPECT_EQ(s1.points_inside, s2.points_inside);
+  EXPECT_EQ(s1.points_total, s2.points_total); 
+  EXPECT_EQ(s1.num_restart + 1, s2.num_restart); 
 }
