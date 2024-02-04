c31bd9b45014a3944a931889415d5da5bc11186e
lemaicirabah <lemaici.rabah@courrier.uqam.ca>
2024-02-03 22:07:39 -0500

    Ajout du redemarrage du calcul montecarlo

--- src/autorestart.c
+++ src/autorestart.c
@@ -1,8 +1,46 @@
+  
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char** argv) {
-  // TODO: implémenter le redémarrage
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <commande>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-  return 0;
+    char* program = argv[1];
+
+    while (1) {
+        printf("Lancement du programme %s...\n", program);
+
+        pid_t pid = fork();
+
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            execvp(program, &argv[1]);
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        } else {
+            int status;
+            waitpid(pid, &status, 0);
+            if (WIFEXITED(status)) {
+                if (WEXITSTATUS(status) == 0) {
+                    printf("Le programme s'est terminé avec succès.\n");
+                    exit(EXIT_SUCCESS);
+                } else {
+                    printf("Le programme a retourné un code d'erreur. Redémarrage automatique dans 5 secondes...\n");
+                }
+            } else {
+                printf("Le programme a planté. Redémarrage automatique dans 5 secondes...\n");
+            }
+
+        }
+    }
+
+    return 0;
 }
+
